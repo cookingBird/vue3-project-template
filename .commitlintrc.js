973c7ba64aafb361ccce119e927f827006ef2e64
Deng Tao <d17381844023@gmail.com>
2024-07-17 19:06:07 +0800

    init

---
+++ .commitlintrc.js
@@ -0,0 +1,11 @@
+import { RuleConfigSeverity } from '@commitlint/types';
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
+  },
+};
