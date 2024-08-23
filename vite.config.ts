6348b5f9c56d038e71daf814484158810eaff194
Deng Tao <d17381844023@gmail.com>
2024-08-23 16:48:09 +0800

    feat: add vite-plugin-html plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from 'vite';
+
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 
+import { createHtmlPlugin } from 'vite-plugin-html';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      eslintrc: {
-        enabled: true,
-      },
-    }),
-    Components({
-      resolvers: [],
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const { VITE_APP_TITLE } = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        eslintrc: {
+          enabled: true,
+        },
+      }),
+      Components({
+        resolvers: [],
+      }),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: VITE_APP_TITLE,
+          },
+        },
+      }),
+    ],
+  };
 });
