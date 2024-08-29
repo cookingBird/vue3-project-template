a2230fc9eb110d40c55423257f8f3a53fad635b2
Deng Tao <d17381844023@gmail.com>
2024-08-29 09:42:02 +0800

    chore: add vite-plugin-html and unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig, loadEnv } from 'vite';
 
 import AutoImport from 'unplugin-auto-import/vite';
+
 import Components from 'unplugin-vue-components/vite';
+
+import IconsResolver from 'unplugin-icons/resolver';
+import Icons from 'unplugin-icons/vite';
 
 import { createHtmlPlugin } from 'vite-plugin-html';
 
@@ -18,8 +22,16 @@ export default defineConfig(({ mode }) => {
           enabled: true,
         },
       }),
+      Icons({
+        compiler: 'vue3',
+        autoInstall: true,
+      }),
       Components({
-        resolvers: [],
+        resolvers: [
+          IconsResolver({
+            prefix: 'icon',
+          }),
+        ],
       }),
       createHtmlPlugin({
         inject: {
